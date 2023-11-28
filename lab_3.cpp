2939cf1a8a15659060b4d856571b9d77f2e7fe8a
Johny <fox.sotiras.work@gmail.com>
2023-11-28 17:09:17 +0300

    Lab 3 Task 3

--- lab_3.cpp
+++ lab_3.cpp
@@ -11,13 +11,20 @@ using std::sort;
 using std::memcpy;
 using std::numeric_limits;
 using std::streamsize;
+using std::string;
+using std::getline;
+using std::stol;
+using std::to_string;
 
 void Prog1();
 void Prog2();
 void Prog3();
 void GetNumbers(mylib::dynamic_array<short>& array);
+void GetString(string& string);
 bool IsEven(short num);
 short GetMinNumber(mylib::dynamic_array<short>& array);
+mylib::dynamic_array<string> SplitString(string input, string delimiter = " ");
+void Convert10ToBaseK(mylib::dynamic_array<string>& array);
 
 int main()
 {
@@ -25,7 +32,7 @@ int main()
     cout << "Какую задачу вы хотите решить?" << endl;
     cout << "  1. Найти минимальное число среди чисел массива." << endl;
     cout << "  2. Ввод строки и её сортировка." << endl;
-    cout << "  3. " << endl;
+    cout << "  3. Преобразование чисел из 10 в 2-16 системы." << endl;
     cout << "  4." << endl;
     cout << "Ваш выбор: ";
     cin >> task;
@@ -60,30 +67,30 @@ void Prog1()
 
 void Prog2()
 {
-    const unsigned short string_len = 512;
+    const unsigned short input_len = 512;
 
-    char string[string_len];
+    char input[input_len];
     cout << "Введите строку: ";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cin.getline(string, string_len);
+    cin.getline(input, input_len);
 
-    char numbers[string_len], symbols[string_len], punctuation[string_len];
+    char numbers[input_len], symbols[input_len], punctuation[input_len];
     unsigned short i = 0, numbers_cnt = 0, symbols_cnt = 0, punctuation_cnt = 0;
-    while(string[i] != '\0')
+    while(input[i] != '\0')
     {
-        if (string[i] >= 48 && string[i] <= 57)
+        if (input[i] >= 48 && input[i] <= 57)
         {
-            numbers[numbers_cnt++] = string[i];
+            numbers[numbers_cnt++] = input[i];
         }
-        else if (string[i] == '!' || string[i] == '"' || string[i] == '\'' || string[i] == '?'
-        || string[i] == '.' || string[i] == ',' || string[i] == '-' || string[i] == ':' || string[i] == ';'
-        || string[i] == '[' || string[i] == ']' || string[i] == '(' || string[i] == ')')
+        else if (input[i] == '!' || input[i] == '"' || input[i] == '\'' || input[i] == '?'
+        || input[i] == '.' || input[i] == ',' || input[i] == '-' || input[i] == ':' || input[i] == ';'
+        || input[i] == '[' || input[i] == ']' || input[i] == '(' || input[i] == ')')
         {
-            punctuation[punctuation_cnt++] = string[i];
+            punctuation[punctuation_cnt++] = input[i];
         }
-        else if (string[i] > 32)
+        else if (input[i] > 32)
         {
-            symbols[symbols_cnt++] = string[i];
+            symbols[symbols_cnt++] = input[i];
         }
 
         ++i;
@@ -93,7 +100,7 @@ void Prog2()
     sort(symbols, symbols + symbols_cnt);
     sort(punctuation, punctuation + punctuation_cnt);
 
-    char output[string_len];
+    char output[input_len];
     memcpy(output, numbers, sizeof(char) * numbers_cnt);
     memcpy(output + numbers_cnt, symbols, sizeof(char) * symbols_cnt);
     memcpy(output + numbers_cnt + symbols_cnt, punctuation, sizeof(char) * punctuation_cnt);
@@ -103,7 +110,34 @@ void Prog2()
 
 void Prog3()
 {
+    string input;
+    GetString(input);
 
+    mylib::dynamic_array<string> string_array = SplitString(input);
+    
+    unsigned short flag = 0;
+    cout << "Желаете перевести полученные числа в систему с другим основанием?" << endl;
+    cout << "  1. Да" << endl << "  2. Нет" << endl;
+    cout << "Ваш выбор: ";
+    cin >> flag;
+    switch (flag)
+    {
+        case 1:
+            Convert10ToBaseK(string_array);
+            break;
+        case 2:
+            break;
+        default:
+            cout << "Ну попросили же 1 или 2..." << endl;
+            break;
+    }
+
+    cout << "Итоговые числа: ";
+    for (unsigned int i = 0; i < string_array.size(); ++i)
+    {
+        cout << string_array[i] << ' ';
+    }
+    cout << endl;
 }
 
 void GetNumbers(mylib::dynamic_array<short>& array)
@@ -147,6 +181,13 @@ void GetNumbers(mylib::dynamic_array<short>& array)
     }
 }
 
+void GetString(string& string)
+{
+    cout << "Введите строку чисел: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, string);
+}
+
 bool IsEven(short num)
 {
     if (num % 2 == 0)
@@ -175,4 +216,73 @@ short GetMinNumber(mylib::dynamic_array<short>& array)
     }
 
     return min;
+}
+
+mylib::dynamic_array<string> SplitString(string input, string delimiter)
+{
+    mylib::dynamic_array<string> array;
+    string temp;
+
+    while (input.find(delimiter) != string::npos)
+    {
+        temp = input.substr(0, input.find(delimiter));
+        array.push_back(temp);
+        input.erase(0, input.find(delimiter) + 1);
+    }
+    array.push_back(input);
+
+    return array;
+}
+
+void Convert10ToBaseK(mylib::dynamic_array<string>& array)
+{
+    unsigned short k = 0;
+    cout << "Какое основание для системы хотите выбрать?" << endl << "Ваш выбор: ";
+    cin >> k;
+
+    long long num = 0;
+    mylib::dynamic_array<unsigned short> digits;
+    string temp;
+    for (unsigned int i = 0; i < array.size(); ++i)
+    {
+        num = stol(array[0]);
+
+        if (num < 0)
+        {
+            temp += '-';
+        }
+
+        while (num != 0)
+        {
+            digits.insert(0, abs(num % k));
+            num /= k;
+        }
+
+        for (unsigned int j = 0; j < digits.size(); ++j) {
+            switch (digits[j])
+            {
+                case 0: temp += '0'; break;
+                case 1: temp += '1'; break;
+                case 2: temp += '2'; break;
+                case 3: temp += '3'; break;
+                case 4: temp += '4'; break;
+                case 5: temp += '5'; break;
+                case 6: temp += '6'; break;
+                case 7: temp += '7'; break;
+                case 8: temp += '8'; break;
+                case 9: temp += '9'; break;
+                case 10: temp += 'A'; break;
+                case 11: temp += 'B'; break;
+                case 12: temp += 'C'; break;
+                case 13: temp += 'D'; break;
+                case 14: temp += 'E'; break;
+                case 15: temp += 'F'; break;
+            }
+        }
+
+        array.push_back(temp);
+        array.erase(0);
+        digits.clear();
+        temp.clear();
+    }
 }
